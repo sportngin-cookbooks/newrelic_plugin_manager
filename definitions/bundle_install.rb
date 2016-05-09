a0ef75b5a321de0287542880492cf1c4891791c2
Brian Bergstrom <brian.bergstrom@sportngin.com>
2016-05-09 14:01:34 -0500

    Use path preference instead of hardcoded path which may not work in opsworks land.

--- definitions/bundle_install.rb
+++ definitions/bundle_install.rb
@@ -5,7 +5,8 @@ define :bundle_install do
   # bundle install
   execute 'bundle install' do
     cwd params[:path]
-    command "/usr/local/bin/bundle install --path #{params[:path]}/.vendor/bundle"
+    command "bundle install --path #{params[:path]}/.vendor/bundle"
+    environment 'PATH' => "/usr/local/bin:/usr/bin:#{ENV['PATH']}"
     user params[:user]
     group params[:group]
     only_if { File.directory?(params[:path]) }
