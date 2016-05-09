166868647016a30c0397291256c92bdf817b6ea2
Brian Bergstrom <brian.bergstrom@sportngin.com>
2016-05-09 13:18:58 -0500

    Use system bundler, not chef embedded bundler.  And fix the damn test.

--- definitions/bundle_install.rb
+++ definitions/bundle_install.rb
@@ -1,14 +1,11 @@
 define :bundle_install do
-  # install bundler gem
-  gem_package 'bundler' do
-    options '--no-ri --no-rdoc'
-    action :upgrade
-  end
+  gem_package 'io-console' # Undeclared dependency of Bundler  1.9.1+
+  gem_package 'bundler'
 
   # bundle install
   execute 'bundle install' do
     cwd params[:path]
-    command "bundle install --path #{params[:path]}/.vendor/bundle"
+    command "/usr/local/bin/bundle install --path #{params[:path]}/.vendor/bundle"
     user params[:user]
     group params[:group]
     only_if { File.directory?(params[:path]) }
